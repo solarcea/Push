2cc713826ec1288c5d22e8208e7149bcb2ecb041
solarcea <165539627+solarcea@users.noreply.github.com>
2024-03-31 09:15:48 +0300

    Create main.py
    The main code of the program

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+from win10toast import ToastNotifier
+
+x = time = 0
+n = ToastNotifier()
+
+a = int(input("Type in how often you want notifications (minutes): "))
+time = a * 60 #making it into minutes
+title = str(input("Type in your title: "))
+msg = str(input("Type in your message: "))
+dur = int(input("Type in how long you want to see it (seconds): "))
+cycle = int(input("Type in how many times you want it to proc: "))
+
+n.show_toast(title, msg, duration = dur)
+
+while x < cycle:
+    time.sleep(t)
+    n.show_toast(title, msg, duration = dur)
+    x += 1
+time.sleep(t+1)
+n.show_toast("Done", duration = 5)
